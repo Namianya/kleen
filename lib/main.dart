a1445d81796449d9432edb43d5e6f9ccb4c6dc23
Namianya <wekesa350@gmail.com>
2022-06-13 03:56:51 +0300

    linking pages to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kleen/home.dart';
+import 'package:kleen/screen13.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Home(),
+      home: const Screen13(),
+      // home: const Screen8(),
+      // home: const Screen5(),
     );
   }
 }
